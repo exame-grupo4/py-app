3c047ff5e90f36d01809fede79d8403aa53f37ea
Gabriel Cruz <cruzg3dev@gmail.com>
2024-12-17 22:50:23 -0300

    remove questionnaire display from resultado function and retain affinity data visualization

--- respostas.py
+++ respostas.py
@@ -8,10 +8,6 @@ def resultado(respostas, cursos_sugeridos, afinidade):
     df_cursos = pd.DataFrame(cursos_sugeridos)
     st.table(df_cursos)
     
-    st.header("Questionário")
-    df_respostas = pd.DataFrame(list(respostas.items()), columns=["Pergunta", "Resposta"])
-    st.table(df_respostas)
-    
     st.header("Afinidade por Área de Conhecimento")
     df_afinidade = pd.DataFrame(list(afinidade.items()), columns=["Área de Conhecimento", "Pontuação"])
     st.bar_chart(df_afinidade.set_index("Área de Conhecimento"))
